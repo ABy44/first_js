52846bbc008f7c296e6e5869fcc5660d2501bc64
ABy44 <aby4492@gmail.com>
2019-01-13 15:13:22 +0300

    Первое задание js

--- test.js
+++ test.js
@@ -1,5 +1,20 @@
+// Первый пункт задания
 let money = prompt("Ваш бюджет на месяц?", "");
-alert(money);
-var time = prompt("Введите дату", "YYYY-MM-DD");
-alert(time);
-// Комментарий
+     time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+// Второй пункт задания
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+// Третий пункт задания
+let question1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    question2 = prompt("Во сколько обойдется?", "");
+appData.expenses.question1;
+appData.expenses.question2;
+// Четвертый пункт задания
+alert(appData.budget / 30);
